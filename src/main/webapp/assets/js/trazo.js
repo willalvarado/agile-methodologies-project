b986a8baec9e5cfb2d358260dcf029073008981a
willalvarado <94190439+willalvarado@users.noreply.github.com>
2023-12-30 21:23:07 -0500

    Update trazo.js

--- src/main/webapp/assets/js/trazo.js
+++ src/main/webapp/assets/js/trazo.js
@@ -15,6 +15,17 @@ document.addEventListener("DOMContentLoaded", function () {
 // Evento de escucha para el clic en el boton
     const btnRed = document.getElementById("btn-red"); //aquí va el id
     btnRed.addEventListener("click", setRed);//aqui la función que cambia el color
+    const btnBlack = document.getElementById("btn-black"); //aquí va el id
+    btnRed.addEventListener("click", setBlack);//aqui la función que cambia el color
+    const btnBlue = document.getElementById("btn-blue"); //aquí va el id
+    btnRed.addEventListener("click", setBlue);//aqui la función que cambia el color
+    const btnPurple = document.getElementById("btn-purple"); //aquí va el id
+    btnRed.addEventListener("click", setPurple);//aqui la función que cambia el color
+    const btnGreen = document.getElementById("btn-green"); //aquí va el id
+    btnRed.addEventListener("click", setGreen);//aqui la función que cambia el color
+    const btnYellow = document.getElementById("btn-yellow"); //aquí va el id
+    btnRed.addEventListener("click", setYellow);//aqui la función que cambia el color
+    
 
 //TODO aregar los otros eventos para los otros botones
 
